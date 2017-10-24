2e7df6c4722dcfbb8aea256ae567fff0d373df78
faltdor <faltdor@gmail.com>
2017-10-24 15:09:02 -0400

    first commit

---
+++ Attendance-API/src/main/java/com/cuemby/attendance/AttendanceApiApplication.java
@@ -0,0 +1,12 @@
+package com.cuemby.attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceApiApplication.class, args);
+	}
+}
